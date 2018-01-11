f4b543413bbc0c68b917dac5093b522c61e7a761
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-01-11 22:46:25 +0300

    First commit

--- format_price.py
+++ format_price.py
@@ -1,5 +1,26 @@
+import argparse
+import locale
+from re import fullmatch
+
+
 def format_price(price):
-    pass
+    locale.setlocale(locale.LC_ALL, ('RU'))
+    if isinstance(price, (int, float)):
+        price = float(price)
+    if isinstance(price, str) and fullmatch('\d*[.,]?\d*$', price):
+        price = float(price.replace(',','.'))
+    if price.is_integer():
+        price_no_fractional = int(format(price, '.0f'))
+        #price_no_fractional = int(locale.format('%d', price))
+        price_comma_delimeter = '{0:,}'.format(price_no_fractional)
+        price_whitespace_delimeter = price_comma_delimeter.replace(',',' ')
+        return price_whitespace_delimeter
+    else:
+        return format(price, '.2f')
+
 
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('price')
+    args = parser.parse_args()
+    print(format_price(args.price))
