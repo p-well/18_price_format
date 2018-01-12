9bd6585a8f10cc9f71b6c18f7a39833c09c5e6ea
Pavel Kadantsev <p.a.kadantsev@gmail.com>
2018-01-12 19:28:16 +0300

    Used string formatting instead locale

--- format_price.py
+++ format_price.py
@@ -3,28 +3,24 @@ from re import fullmatch
 
 
 def format_price(price):
-    if isinstance(price, (int, float)):
+    if isinstance(price, int) or isinstance(price, float):
         price = float(price)
-    if isinstance(price, str) and fullmatch('\d*[.,]?\d*$', price):
+    elif isinstance(price, str) and fullmatch('\d*[.,]?\d*$', price):
         price = float(price.replace(',','.'))
-    if price.is_integer():
-        price_no_fractional = int('{:08.0f}'.format(price))
-        print(price_no_fractional, len(str(price_no_fractional)))
-        #price_no_fractional = int(locale.format('%d', price))
-        price_comma_delimeted = '{:,}'.format(price_no_fractional)
+        if price.is_integer():
+            price_no_decimal = int('{:.0f}'.format(price))
+            price_comma_delimeted = '{:,}'.format(price_no_decimal)
+        else:
+            price_2_decimals = float('{:.2f}'.format(price))
+            price_comma_delimeted = '{:,}'.format(price_2_decimals)
         price_whitespace_delimeted = price_comma_delimeted.replace(',',' ')
         return price_whitespace_delimeted
     else:
-        return format(price, '.2f')
+        raise ValueError("Wrong input")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('price')
     args = parser.parse_args()
-print(format_price(args.price))
-
-#The ',' option signals the use of a comma for a thousands separator. For a locale aware separator, use the 'n' integer presentation type instead.
-
-#6.1.3.2
-#In most of the cases the syntax is similar to the old %-formatting, with the addition of the {} and with : used instead of %. For example, '%03.2f' can be translated to '{:03.2f}'.
+    print(format_price(args.price))
