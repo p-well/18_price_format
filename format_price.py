5ad0c84e3a75f7b5dba0b8733228aae62c3c445d
Pavel <p.a.kadantsev@gmail.com>
2018-01-12 14:59:33 +0300

    Draft

--- format_price.py
+++ format_price.py
@@ -1,20 +1,19 @@
 import argparse
-import locale
 from re import fullmatch
 
 
 def format_price(price):
-    locale.setlocale(locale.LC_ALL, ('RU'))
     if isinstance(price, (int, float)):
         price = float(price)
     if isinstance(price, str) and fullmatch('\d*[.,]?\d*$', price):
         price = float(price.replace(',','.'))
     if price.is_integer():
-        price_no_fractional = int(format(price, '.0f'))
+        price_no_fractional = int('{:08.0f}'.format(price))
+        print(price_no_fractional, len(str(price_no_fractional)))
         #price_no_fractional = int(locale.format('%d', price))
-        price_comma_delimeter = '{0:,}'.format(price_no_fractional)
-        price_whitespace_delimeter = price_comma_delimeter.replace(',',' ')
-        return price_whitespace_delimeter
+        price_comma_delimeted = '{:,}'.format(price_no_fractional)
+        price_whitespace_delimeted = price_comma_delimeted.replace(',',' ')
+        return price_whitespace_delimeted
     else:
         return format(price, '.2f')
 
@@ -23,4 +22,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('price')
     args = parser.parse_args()
-    print(format_price(args.price))
+print(format_price(args.price))
+
+#The ',' option signals the use of a comma for a thousands separator. For a locale aware separator, use the 'n' integer presentation type instead.
+
+#6.1.3.2
+#In most of the cases the syntax is similar to the old %-formatting, with the addition of the {} and with : used instead of %. For example, '%03.2f' can be translated to '{:03.2f}'.
