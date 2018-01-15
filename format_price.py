217edfeae04387244792b0aeb2ce8fa68afb4e63
Pavel <p.a.kadantsev@gmail.com>
2018-01-15 21:43:02 +0300

    Minor updates

--- format_price.py
+++ format_price.py
@@ -2,17 +2,19 @@ import argparse
 import re
 
 
-def format_price(price):
+def format_price_in_local(price):
     price_template = '\d*[.,]?\d*$'
     price_template_with_quotes = '[\',\"]\d*[.,]?\d*[\',\"]$'
     if any([
-            re.fullmatch(price_template, price),
-            re.fullmatch(price_template_with_quotes, price)
+            re.fullmatch(price_template, str(price)),
+            re.fullmatch(price_template_with_quotes, str(price))
        ]):
-        price = re.search(r'\d*[.,]?\d{3}', price).group(0).replace(',','.')
+        price = re.search(
+            r'\d*[.,]?\d{0,3}',
+            str(price)
+        ).group(0).replace(',','.')
         price = float(price)
         if price.is_integer():
-            
             price_comma_delimeted = '{:,.0f}'.format(price) #no decimal
         else:
             price_comma_delimeted = '{:,.2f}'.format(price) #2 decimals
@@ -25,5 +27,5 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('price')
     arguments = parser.parse_args()
-    local_price = format_price(arguments.price)
+    local_price = format_price_in_local(arguments.price)
     print(local_price)
