02546720a8bb4cbb917eac46e13a8adca498b03e
Pavel <p.a.kadantsev@gmail.com>
2018-01-15 17:50:49 +0300

    Format directly in {:,.0f} for int and {:,.2f} for not int

--- format_price.py
+++ format_price.py
@@ -1,26 +1,31 @@
 import argparse
-from re import fullmatch
+import re
 
 
 def format_price(price):
-    if isinstance(price, int) or isinstance(price, float):
+    price_template = '\d*[.,]?\d*$'
+    price_template_with_quotes = '[\',\"]\d*[.,]?\d*[\',\"]$'
+    if any([
+            re.fullmatch(price_template, price),
+            re.fullmatch(price_template_with_quotes, price)
+       ]):
+        price = re.search(r'\d*[.,]?\d{3}', price).group(0).replace(',','.')
         price = float(price)
-    elif isinstance(price, str) and fullmatch('\d*[.,]?\d*$', price):
-        price = float(price.replace(',','.'))
+        print(price)
         if price.is_integer():
-            price_no_decimal = int('{:.0f}'.format(price))
-            price_comma_delimeted = '{:,}'.format(price_no_decimal)
+            print('price is integer')
+            price_comma_delimeted = '{:,.0f}'.format(price) #no decimal
         else:
-            price_2_decimals = float('{:.2f}'.format(price))
-            price_comma_delimeted = '{:,}'.format(price_2_decimals)
+            print('price is not integer')
+            price_comma_delimeted = '{:,.2f}'.format(price) #2 decimals
         price_whitespace_delimeted = price_comma_delimeted.replace(',',' ')
-        return price_whitespace_delimeted
+        print(price_whitespace_delimeted)
     else:
-        raise ValueError("Wrong input")
+        raise ValueError('Wrong price format')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('price')
-    args = parser.parse_args()
-    print(format_price(args.price))
+    arguments = parser.parse_args()
+    local_price = format_price(arguments.price)
